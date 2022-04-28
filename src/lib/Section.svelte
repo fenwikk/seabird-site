d2c0b21f0c1dbd09fe65896bf123aefb6bb3daf8
Filip Mårtensson <fenwikk@protonmail.com>
2022-04-28 21:46:41 +0200

    Added classes to Section component

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
-    export let bgClass: string
-    export let overlayClass: string
+    export let bgClass: string = ""
+    export let overlayClass: string = ""
 
     export let paddingClass: string = "md:px-36 py-44"
+    
+    let classes: string = ""
+    export { classes as class}
 </script>
 
 <div class="w-full {bgClass}">
     <div class={overlayClass}>
-        <div class="container {paddingClass}">
+        <div class="container {paddingClass} {classes}">
             <slot></slot>
         </div>
     </div>
