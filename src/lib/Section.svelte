574242ad5bf7ba505239147c9b7069b1f84fd7fa
Filip Mårtensson <fenwikk@protonmail.com>
2022-04-28 15:54:55 +0200

    Added Section component

---
+++ src/lib/Section.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let bgClass: string
+    export let overlayClass: string
+
+    export let paddingClass: string = "md:px-36 py-44"
+</script>
+
+<div class="w-full {bgClass}">
+    <div class={overlayClass}>
+        <div class="container {paddingClass}">
+            <slot></slot>
+        </div>
+    </div>
+</div>
