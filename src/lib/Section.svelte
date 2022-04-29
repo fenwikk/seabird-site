987a175e565643ff063699df51943c7e0fee79e1
Filip Mårtensson <fenwikk@protonmail.com>
2022-04-29 23:56:32 +0200

    Parallax Hero

--- src/lib/Section.svelte
+++ src/lib/Section.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import { scrollTo } from "svelte-scrollto"
+
     export let bgClass: string = ""
     export let overlayClass: string = ""
 
@@ -7,12 +9,28 @@
     let classes: string = ""
     let sectionId: string = ""
     export { classes as class, sectionId as id}
+
+    export let parallax: boolean = false
+
+    let parallaxStyle: string[] = []
+    const applyParallax = (e: MouseEvent) => {
+        if (parallax) {
+            let w = window.innerWidth/2;
+            let h = window.innerHeight/2;
+            let mouseX = e.clientX;
+            let mouseY = e.clientY;
+
+            parallaxStyle = [`background-position: ${50 - (mouseX - w) * 0.01}% ${50 - (mouseY - h) * 0.01}%; background-size: 105%;`, `right: ${(mouseX - w) * 0.005}px; bottom: ${(mouseY - h) * 0.01}px;`];
+        }
+    }
 </script>
 
-<div class="w-full {bgClass}" id={sectionId}>
+<div on:mousemove={applyParallax} class="w-full {bgClass}" id={sectionId} style={parallaxStyle[0]}>
     <div class={overlayClass}>
-        <div class="container {paddingClass} {classes}">
-            <slot></slot>
+        <div class="container relative {paddingClass} {classes}" style={parallaxStyle[1]}>
+                <slot></slot>
         </div>
+
+        <buttton on:click={() => { scrollTo({ element: "#uc" }) }} class="icon-scroll cursor-pointer"></buttton>
     </div>
 </div>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,100 +1,10 @@
 <script lang="ts">
-    import { scrollTo } from "svelte-scrollto"
-
     import underConstruction from "$lib/assets/under-construction.svg"
 
     import Section from "$lib/Section.svelte"
-    import { Hamburger } from "svelte-hamburgers"
 
     let open = false
 </script>
-
-<style>
-    .icon-scroll,
-    .icon-scroll:before {
-        position: absolute;
-        left: 50%;
-        bottom: 2rem;
-    }
-    .icon-scroll {
-        width: 30px;
-        height: 52.5px;
-        margin-left: -15px;
-        margin-top: -26.25px;
-        box-shadow: inset 0 0 0 1px #fff;
-        border-radius: 20px;
-    }
-    .icon-scroll:before {
-        content: '';
-        width: 4px;
-        height: 4px;
-        background: #fff;
-        margin-left: -2px;
-        top: 8px;
-        border-radius: 3px;
-        animation-duration: 2.5s;
-        animation-iteration-count: infinite;
-        animation-name: scroll;
-    }
-    @-moz-keyframes scroll {
-        0% {
-            opacity: 1;
-        }
-        75% {
-            opacity: 0;
-            transform: translateY(32.5px);
-        }
-        100% {
-            opacity: 0;
-            transform: translateY(32.5px);
-        }
-    }
-    @-webkit-keyframes scroll {
-        0% {
-            opacity: 1;
-        }
-        75% {
-            opacity: 0;
-            transform: translateY(32.5px);
-        }
-        100% {
-            opacity: 0;
-            transform: translateY(32.5px);
-        }
-    }
-    @-o-keyframes scroll {
-        0% {
-            opacity: 1;
-        }
-        75% {
-            opacity: 0;
-            transform: translateY(32.5px);
-        }
-        100% {
-            opacity: 0;
-            transform: translateY(32.5px);
-        }
-    }
-    @keyframes scroll {
-        0% {
-            opacity: 0;
-            transform: translateY(4px);
-        }
-        25% {
-            opacity: 1;
-            transform: translateY(-2px);
-        }
-        75% {
-            opacity: 0;
-            transform: translateY(32.5px);
-        }
-        100% {
-            opacity: 0;
-            transform: translateY(32.5px);
-        }
-    }
-
-</style>
 
 <svelte:head>
     <title>Seabird Digital | Modern webbdesign till ett bra pris nära Göteborg</title>
@@ -121,14 +31,12 @@
     </div>
 </div>
 
-<Section bgClass="bg-hero bg-cover" overlayClass="bg-sb-black/70" class="flex justify-center items-center h-screen relative">
+<Section bgClass="bg-hero" overlayClass="bg-sb-black/70" class="flex justify-center items-center h-screen relative" parallax>
     <h1>
         <span class="font-quicksand text-white text-2xl font-normal relative top-11 left-4">Modern</span><br>
         <span class="font-brush text-white text-9xl font-normal">webbdesign</span><br>
         <span class="font-scope text-white text-4xl font-thin relative bottom-8 left-12">till ett bra pris</span>
     </h1>
-
-    <buttton on:click={() => { scrollTo({ element: "#uc" }) }} class="icon-scroll cursor-pointer"></buttton>
 </Section>
 
 <Section id="uc" class="flex flex-col items-center">
